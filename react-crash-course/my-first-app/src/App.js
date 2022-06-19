bd8ccdebaeb36dd3ef9e0b4cfb099146c61932ba
francisojeah <ojeahfrancis@gmail.com>
2022-06-19 17:17:16 +0100

    Finished the react crash course projects

--- react-crash-course/my-first-app/src/App.js
+++ react-crash-course/my-first-app/src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import HelloWorld from './components/HelloWorld';
-import HelloWorld2 from './components/HelloWorld2';
-import HelloWorld3 from './components/HelloWorld3';
-
+import './App.css'
+//import HelloWorld from './components/HelloWorld';
+//import HelloWorld2 from './components/HelloWorld2';
+//import HelloWorld3 from './components/HelloWorld3';
+//import HelloWorld4 from './components/HelloWorld4';
+//import HelloWorld5 from './components/HelloWorld5';
+//import HelloWorld6 from './components/HelloWorld6/HelloWorld6';
+//import HelloWorld7 from './components/HelloWorld7/HelloWorld7';
+//import HelloWorld8 from './components/HelloWorld8';
 const App = () => {
   return (
-    <div>
-      <HelloWorld />
-      <HelloWorld2 name="Francis" />
-      <HelloWorld2 />
-      <HelloWorld3 name="Oluebube" />
+    <div className="Container">
+      <div className="Column1">
+        <HelloWorld8 />
+      </div>
+      <div className="Column2">
+        <HelloWorld4 />
+      </div>
+      <div className="Column3">
+        <HelloWorld7 />
+      </div>
     </div>
-  );
+    );
+  //return (
+    //<div>
+      //<HelloWorld />
+      //<HelloWorld2 name="Francis" />
+      //<HelloWorld2 />
+      //<HelloWorld3 name="Oluebube" />
+      //<HelloWorld4 />
+      //<HelloWorld5 />
+      //<HelloWorld6 />
+      //<HelloWorld7 />
+      //<HelloWorld8 />
+    //</div>
+  //);
 }
 export default App;
