249af8eed93f17ef8ed3ff6b31f3dbe65ec20d38
francisojeah <ojeahfrancis@gmail.com>
2022-06-19 17:19:15 +0100

    Made corrections to App.js file

--- react-crash-course/my-first-app/src/App.js
+++ react-crash-course/my-first-app/src/App.js
@@ -3,11 +3,11 @@ import './App.css'
 //import HelloWorld from './components/HelloWorld';
 //import HelloWorld2 from './components/HelloWorld2';
 //import HelloWorld3 from './components/HelloWorld3';
-//import HelloWorld4 from './components/HelloWorld4';
+import HelloWorld4 from './components/HelloWorld4';
 //import HelloWorld5 from './components/HelloWorld5';
 //import HelloWorld6 from './components/HelloWorld6/HelloWorld6';
-//import HelloWorld7 from './components/HelloWorld7/HelloWorld7';
-//import HelloWorld8 from './components/HelloWorld8';
+import HelloWorld7 from './components/HelloWorld7/HelloWorld7';
+import HelloWorld8 from './components/HelloWorld8';
 const App = () => {
   return (
     <div className="Container">
